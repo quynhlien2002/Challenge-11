e0dda7bcda35a078e5e646372e157cda304c9407
Daisy Lien <daisylien1211@gmail.com>
2022-09-20 10:53:21 -0700

    change

--- Develop/server.js
+++ Develop/server.js
@@ -6,9 +6,8 @@
 
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
-// const req = require('express/lib/request');
-const note = require('./db.json');
+// const fs = require('fs');
+const note = require('./db/db.json');
 
 const PORT = process.env.port || 3000;
 const app = express();
@@ -18,18 +17,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 })
 
 app.get('/note', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'))
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
 
 app.get('/api/notes/', (req, res) => {
     res.json(note);
 })
 
-app.post('/api/notes', (req, res) => {
+app.post('/api/notes/', (req, res) => {
     console.info(`${req.method} request received to add a review `)
 
     const { title, task } = req.body;
@@ -54,10 +53,8 @@ app.post('/api/notes', (req, res) => {
 })
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 });
-
-
 
 
 app.listen(PORT, () => {
