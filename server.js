d00604f1bfe428043fd803102f9a9c7893c9be34
Daisy Lien <daisylien1211@gmail.com>
2022-09-20 11:10:45 -0700

    change

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 })
 
-app.get('/note', (req, res) => {
+app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
 
