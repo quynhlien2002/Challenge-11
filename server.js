70972e2744c108fa19356e1259b91bf224d56a81
Daisy Lien <daisylien1211@gmail.com>
2022-09-20 11:24:05 -0700

    heroku

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const path = require('path');
 // const fs = require('fs');
 const note = require('./db/db.json');
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
